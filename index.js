ed7e1db4cd9676bd7e0fa8b7244dafc468aa8e5a
Himanshu916 <hranaritsh@gmail.com>
2020-12-05 05:48:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+for(var i=0;i<5;i++)
+{
+  var str="";
+  for(var j=0;j<=i;j++)
+  {
+    str += "*";
+   
+  }
+   console.log(str)
+}
+console.log("");
+console.log("now inverted");
+console.log("");
+
+
+
+// inverted
+for(var i=5;i>0;i--)
+{
+  var str="";
+  for(var j=0;j<i;j++)
+  {
+    str += "*";
+   
+  }
+   console.log(str)
+}
